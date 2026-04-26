5e3457f6b5a36f8eeead5bd9ab72418746ad8003
mustafayazin428-cmyk <mustafayazin428@gmail.com>
2026-04-27 00:36:40 +0300

    run.py

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
 from gevent import monkey
 monkey.patch_all()
 
-from backend.app import socketio, app
+from app import socketio, app
 
 if __name__ == '__main__':
-    print("✅ Server running on http://localhost:5000")
     socketio.run(app, debug=False, port=5000)
